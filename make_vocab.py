6b31dbd7d571a7a35ed9b30981cf731b3fbfd211
vvv-tech <kronizzyd@gmail.com>
2023-02-26 14:53:24 +0300

    useless piece of code removed

--- make_vocab.py
+++ make_vocab.py
@@ -1,8 +1,5 @@
 from glob import glob
-import random
 import os
-
-random.seed(17)
 
 def make_vocab(txts_dir):
     """
@@ -11,9 +8,6 @@ def make_vocab(txts_dir):
     texts_combined = ""
     txts_dir = os.path.join(txts_dir, "*")
     txts_filenames = glob(txts_dir)
-    # only need one tenth selected randomly
-    needed_k = len(txts_filenames) // 10
-    random.choices(txts_filenames, k=needed_k)
 
     for txt_filename in txts_filenames:
         with open(txt_filename, "r", errors="ignore") as f:
@@ -25,4 +19,4 @@ def make_vocab(txts_dir):
     print("Vocab is \"", vocab, "\"")
     return vocab
 
-#make_vocab("./pdfs_txts/")
+make_vocab("./pdfs_txts/")
