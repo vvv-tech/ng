bb87f433ca6ddd09e52e88da68d53296ddd5d44c
vvv-tech <kronizzyd@gmail.com>
2023-02-28 09:48:56 +0300

    get batches of data

---
+++ get_batch.py
@@ -0,0 +1,9 @@
+import torch
+
+torch.manual_seed(17)
+
+def get_batch(data, batch_size=4, block_size=8):
+    ix = torch.randint(len(data) - block_size, (batch_size,))
+    x = torch.stack([data[i:i+block_size] for i in ix])
+    y = torch.stack([data[i+1:i+block_size+1] for i in ix])
+    return x, y
